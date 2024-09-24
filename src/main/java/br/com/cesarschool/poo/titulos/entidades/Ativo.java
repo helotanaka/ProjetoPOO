018cfa1a3b5ba1611570bbfa6128bbb6c6a904b8
Tanaka <130470569+helotanaka@users.noreply.github.com>
2024-09-24 09:40:05 -0300

    Initial commit

---
+++ src/main/java/br/com/cesarschool/poo/titulos/entidades/Ativo.java
@@ -0,0 +1,45 @@
+package br.com.cesarschool.poo.titulos.entidades;
+
+import java.time.LocalDate;
+
+/*
+ * Esta classe deve ter os seguintes atributos:
+ * identificador, do tipo int OK
+ * nome, do tipo String OK
+ * data de validade, do tipo LocalDate OK
+ * 
+ * Deve ter um construtor p�blico que inicializa os atributos, OK
+ * e m�todos set/get p�blicos para os atributos. O atributo identificador
+ * � read-only fora da classe.OK
+ */
+public class Ativo {
+    private int identificador;
+    private String nome;
+    private LocalDate dataDeValidade;
+
+    public Ativo(int identificador, String nome, LocalDate dataDeValidade){
+        this.identificador = identificador;
+        this.nome = nome;
+        this.dataDeValidade = dataDeValidade;
+    }
+
+    public int getIdentificador(){
+        return identificador;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataDeValidade() {
+        return dataDeValidade;
+    }
+
+    public void setDataDeValidade(LocalDate dataDeValidade){
+        this.dataDeValidade = dataDeValidade;
+    }
+}
---
+++ src/main/java/br/com/cesarschool/poo/titulos/entidades/TituloDivida.java
@@ -0,0 +1,28 @@
+package br.com.cesarschool.poo.titulos.entidades;
+
+import java.time.LocalDate;
+
+/*
+ * Esta classe deve herdar de Ativo.
+ * E deve ter os seguintes atributos:
+ * taxaJuros, do tipo double.
+ * 
+ * Deve ter um construtor p�blico que inicializa os atributos, 
+ * e m�todos set/get p�blicos para os atributos.
+ * 
+ * Deve ter um m�todo p�blico double calcularPrecoTransacao(double montante): o pre�o 
+ * da transa��o � montante vezes (1 - taxaJuros/100.0).
+ */
+public class TituloDivida extends Ativo{
+    private double taxaJuros;
+
+    public TituloDivida(int identificador, String nome, LocalDate dataDeValidade, double taxaJuros){
+        super(identificador, nome, dataDeValidade);
+        this.taxaJuros = taxaJuros;
+    }
+
+    public double calcularPrecoTransacao(double montante){
+        return montante * (1 - taxaJuros/100.0);
+    }
+
+}
